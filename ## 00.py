19a46cfccc164b012fc7650452f3dad91c7fe255
Shreshth Dadhich <165061450+Shreshth-Dadhich@users.noreply.github.com>
2026-07-12 19:28:31 +0400

    Enhance PyTorch fundamentals with tensor operations
    Updated the PyTorch fundamentals notebook with additional tensor operations and explanations. These are the reinforced branches that I have learned.

--- ## 00.py
+++ ## 00.py
@@ -36,6 +36,7 @@ print(tensor)
 print(tensor.ndim) # 3-d tensor
 print(tensor.shape) # shape of the tensor
 
+
 ### Random Tensors
 # Random tensors are tensors of arbitrary size filled with random numbers.
 random_tensor = torch.rand(3, 4)
@@ -47,7 +48,8 @@ print(random_tensor.ndim) # 2-d tensor
 random_image_size_tensor = torch.rand(224, 224, 3) # height, width, color channels
 print(random_image_size_tensor.shape, random_image_size_tensor.ndim) # shape and dimension of the tensor
 
-#Tensor of zeros
+
+#Tensor of zeros or ones
 #Torch zeros tensor - https://pytorch.org/docs/stable/generated/torch.zeros.html
 ones_tensor = torch.ones(3, 4)
 print(ones_tensor)
@@ -62,16 +64,72 @@ print(Arranged_tensor) # 1-d tensor from 1 to 10
 #Creating a tensor-like of another tensor
 tensor_zeros_like = torch.zeros_like(Arranged_tensor) # create a tensor of zeros with the same shape as random_tensor
 print(tensor_zeros_like) 
-"""
 
 ##Tensor Datatypes
 #Tensors can have different datatypes. The default datatype is float32.
 #recuires_grad=False means that the tensor will not be tracked for gradients during backpropagation.
 float_32_tensor = torch.tensor([3.0, 6.0, 9.0], dtype=torch.float32, device=None, requires_grad=False)
 print(float_32_tensor.dtype) 
+"""
 
 """ The three main issues that i'll probably run into when working with pytorch & deep learning are:
 1. Tensor datatypes
 2. Tensor shapes
 3. Tensor device (CPU vs GPU)
 """
+
+"""
+# Manipulating tensors (tensor operations)
+Tensor operations include :
+    addition   subtraction   multiplication(element-wise)   division(element-wise)   matrix multiplication   reshaping   indexing/slicing   aggregation (sum, mean, etc.)   broadcasting
+"""
+"""
+tensor = torch.tensor([1,2,3,4,5])
+
+#ADDING TENSOR
+print(tensor+10) 
+
+#SUBTRACTING TENSOR
+print(tensor-10)
+
+#MULTIPLYING TENSOR
+print(tensor*10)
+
+#DIVIDING TENSOR
+print(tensor/10)
+"""
+
+"""
+When performing matrix multiplication---
+
+the inner dimensions must be the same.:
+(3,2) @ (3,2) will not work
+(3,2) @ (2,3) will work
+(2,3) @ (3,2) will work
+
+The output shape will be the outer dimensions of the two matrices being multiplied.
+(3,2) @ (2,3) = (3,3)
+
+To perform matrix multiplication in PyTorch, you can use the @ operator or the torch.matmul() function.
+"""
+Tensor_A = torch.tensor([[1, 2], 
+                         [3, 4], 
+                         [5, 6]]) # shape (3,2)
+
+Tensor_B = torch.tensor([[7, 8], 
+                         [9, 10], 
+                         [11, 12]]) # shape (3,2)
+
+
+"""
+Whem we multiply Tensor_A and Tensor_B, we get a runtime error; To fix this, we can transpose one of the tensors to make the inner dimensions match.
+    A transpose switches the axes of a inputed tensor.
+    To transpose a tensor in PyTorch, you can use the .T attribute or the torch.transpose() function.
+
+Tensor_B_T = Tensor_B.T 
+tensor_matmul_AB = torch.matmul(Tensor_A, Tensor_B_T)
+print(tensor_matmul_AB, '\n', tensor_matmul_AB.shape)
+"""
+
+
+
